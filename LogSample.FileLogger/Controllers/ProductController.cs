65b44d0d99f74df9445daa985f7d62cb12a7bfce
Kamyab Rostami <kamyabrostamii@gmail.com>
2022-07-21 21:40:21 +0430

    Using Log Provider

---
+++ LogSample.FileLogger/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LogSample.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductController : ControllerBase
+{
+    private readonly ILogger<ProductController> _logger;
+    private readonly ILogger _logger2;
+
+    public ProductController(ILogger<ProductController> logger, ILoggerFactory loggerFactory)
+    {
+        _logger = logger;
+        _logger2 = loggerFactory.CreateLogger("ProductComponents");
+    }
+
+    [HttpGet("/Product")]
+    public ActionResult GetProduct(int id)
+    {
+        _logger.LogInformation("Request for Product With {ID} Responed",id);
+        _logger2.LogInformation("Request for Product With {ID} Responed",id);
+        return Ok(new
+        {
+            ProductId = id,
+            ProductName = $"Product {id}"
+        });
+    }
+}
+
